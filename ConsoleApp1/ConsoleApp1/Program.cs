a71d4b26d8fb474db8277bb71703063fa77186a0
m11jr <alinurmammadzada@gmail.com>
2024-11-06 09:31:37 +0100

    Three Labs Are Here

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("I am " + (2024 - 2005) + " Years Old");
+            Console.WriteLine("I am {0} Years Old",2024-2005);
+            Console.WriteLine($"I am {2024-2005} years old");
+            Console.WriteLine($" This is \\ and this is \" ");
+            Console.Write("hello");
+            Console.WriteLine("\nThis is My World");
+            Console.WriteLine("***********************");
+            Console.WriteLine("Name \t \t Surname");
+            Console.WriteLine("Alinur \t \t Mammadzada");
+            Console.WriteLine("Alinur \t \t Mammadzada");
+            Console.WriteLine(" I am from \bPoland");
+            //\b helps you to delete the last letter before itself
+            Console.WriteLine("This is an alarm \a");
+
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age;
+            //Console.Write("When did you born: [    ]\b\b\b\b\b");
+            //age = Convert.ToInt32(Console.ReadLine());
+            ////Int is 4 bit ==== 4* 8 bits = 32 bits So Convert.ToInt32
+            //// If it is short age then it will be 2*8bits Convert.ToInt16;
+            //Console.WriteLine("*********************");
+            //Console.WriteLine($"You are {2024 - age}");
+            //Console.WriteLine("*********************");
+
+
+            Console.WriteLine("******************ExPargo Electronics******************");
+            Console.Write("How Much TV Do you Want [ ]\b\b");
+            int NumberTV = Convert.ToInt32(Console.ReadLine());
+
+
+            int CostTVWT = 1000 * NumberTV;
+            int CostTVT = CostTVWT + (CostTVWT*4/100);
+            Console.WriteLine($"One TV is 1000 Zloty. So The cost is : [{CostTVWT}]");
+            Console.WriteLine($"Tax Rate is 4% So Total costs of TV is [{CostTVT}]");
+            Console.Write("How much to your Home From Here [   ]\b\b\b\b");
+            int distance = Convert.ToInt32(Console.ReadLine());
+            int DistanceCost = distance * 10;
+            Console.WriteLine($"Total Cost is : {CostTVT+DistanceCost} Zloty");
+            Console.WriteLine("*************************************");
+
+
+
+           
+        }
+    }
+}
